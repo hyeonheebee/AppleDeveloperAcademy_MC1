14a43f0556f3f6222b92be27148816c5bc81d264
Hyeonhee <hyunheeshim0103@gmail.com>
2024-03-22 14:20:49 +0900

    Initial Commit

---
+++ ADA_MC1/ADA_MC1App.swift
@@ -0,0 +1,17 @@
+//
+//  ADA_MC1App.swift
+//  ADA_MC1
+//
+//  Created by ShimHyeonhee on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ADA_MC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ADA_MC1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ADA_MC1
+//
+//  Created by ShimHyeonhee on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
